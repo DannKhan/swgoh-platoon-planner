5e4fce35d49da649b5c4fc933933ffb8c8a4cdfb
DannKhan <DannKhan@mail.ru>
2026-07-21 14:39:51 +0300

    Change const names to ..Response

--- src/snapshot-builder/builder.js
+++ src/snapshot-builder/builder.js
@@ -2,15 +2,16 @@ import { getPlayerByAllyCode } from "../providers/comlink/player.js";
 import { getGuild } from "../providers/comlink/guild.js";
 
 export async function buildGuildSnapshot(allyCode) {
-    const player = await getPlayerByAllyCode(allyCode);
-    const guildResponse = await getGuild(player.guildId);
 
-    const snapshot = {
+    const playerResponse = await getPlayerByAllyCode(allyCode);
+    const guildId = playerResponse.guildId;
+
+    const guildResponse = await getGuild(guildId);
+
+    return {
         snapshotTime: new Date().toISOString(),
         source: "Comlink",
         guild: guildResponse.guild,
         players: []
-};
-
-return snapshot;
+    };
 }
